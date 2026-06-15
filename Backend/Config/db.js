cd8b390aed04fe0b1bb23b5812b33e0ea20138f2
amrutagavade04 <amrutagavade2004@gmail.com>
2026-06-15 16:26:20 +0530

    debug db url

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -3,11 +3,15 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
+    console.log("========== DEBUG ==========");
     console.log("DB_URL:", process.env.DB_URL);
+    console.log("===========================");
+
     await mongoose.connect(process.env.DB_URL);
+
     console.log("Database connected successfully ✅");
   } catch (error) {
-    console.error("Mongoose connection failed:", error.message);
+    console.error("Mongoose connection failed:", error);
     process.exit(1);
   }
 };
